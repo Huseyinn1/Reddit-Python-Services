11dea67edbd7f7a5ff5763cecdd8d20c6c337a0e
Hüseyin Adıgüzel <ahuseyin235@gmail.com>
2023-06-22 12:58:06 +0300

    Refactored reddit_crawler.py with SQLAlchemy ORM for improved performance

--- reddit_crawler.py
+++ reddit_crawler.py
@@ -1,9 +1,20 @@
 import praw
-import sqlite3
 import time
+from sqlalchemy import create_engine, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(String, primary_key=True)
+    title = Column(String)
+    content = Column(String)
 
 def crawl_posts(stop_event):
-    #login işlemi
+    # Login bilgileri
     client_id = 'client_id'
     client_secret = 'client_secret'
     username = 'username'
@@ -12,12 +23,11 @@ def crawl_posts(stop_event):
     # Takip edilecek subreddit adı
     subreddit_name = 'cybersecurity'
     
-    conn = sqlite3.connect('reddit_posts.db')
-    c = conn.cursor()
-    
-    c.execute('''CREATE TABLE IF NOT EXISTS posts
-                 (id TEXT PRIMARY KEY, title TEXT, content TEXT)''')
-    conn.commit()
+    # Veritabanı bağlantısı ve oturum oluşturma
+    engine = create_engine('sqlite:///reddit_posts.db')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     
     # Reddit'e bağlanma
     reddit = praw.Reddit(client_id=client_id,
@@ -26,34 +36,26 @@ def crawl_posts(stop_event):
                          password=password,
                          user_agent='my_user_agent')
     
-   
-
     # Sürekli olarak yeni postları kontrol etme
     while not stop_event :
         try:
-            
             subreddit = reddit.subreddit(subreddit_name)
-            new_posts = subreddit.new(limit=10)  # Son 10 postu alabiliriz isteğe bağlı olarak değiştirilebilir.
+            new_posts = subreddit.new(limit=10)  # Son 10 postu alabiliriz, isteğe bağlı olarak değiştirilebilir.
     
             # Her post için işlemleri yap
             for post in new_posts:
-                
-                c.execute('SELECT * FROM posts WHERE id = ?', (post.id,))
-                existing_post = c.fetchone()
+                existing_post = session.query(Post).filter_by(id=post.id).first()
     
                 if not existing_post:
-                    
-                    c.execute('INSERT INTO posts VALUES (?, ?, ?)', (post.id, post.title, post.selftext))
+                    new_post = Post(id=post.id, title=post.title, content=post.selftext)
+                    session.add(new_post)
+                    session.commit()
                     print(f'Yeni post eklendi: {post.title}')
-    
-            
-            conn.commit()
     
             # Belirli bir süre sonra tekrar kontrol etme
             time.sleep(5)
         except Exception as e:
             print(f'Hata oluştu: {e}')
+            session.rollback()
     
-    
-    conn.close()
-
+    session.close()
