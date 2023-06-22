c9173214bf4e813ee073b776eee5b58746bc4a0e
Hüseyin Adıgüzel <ahuseyin235@gmail.com>
2023-06-22 12:52:40 +0300

    Refactored api.py with SQLAlchemy ORM

--- api.py
+++ api.py
@@ -1,30 +1,42 @@
 from flask import Flask, jsonify
-import sqlite3
-
+from sqlalchemy import create_engine, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 
-@app.route('/api/posts')
+Base = declarative_base()
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(String, primary_key=True)
+    title = Column(String)
+    content = Column(String)
+
 def get_posts():
-    
-    conn = sqlite3.connect('reddit_posts.db')
-    c = conn.cursor()
-    
-    c.execute('SELECT * FROM posts')
-    rows = c.fetchall()
-    
-    posts = []
-    for row in rows:
-        post = {
-            'id': row[0],
-            'title': row[1],
-            'content': row[2]
+    engine = create_engine('sqlite:///reddit_posts.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    posts = session.query(Post).all()
+
+    post_list = []
+    for post in posts:
+        post_dict = {
+            'id': post.id,
+            'title': post.title,
+            'content': post.content
         }
-        posts.append(post)
-    
-    conn.close()
-    
-    return jsonify(posts)
+        post_list.append(post_dict)
+
+    session.close()
+
+    return jsonify(post_list)
+
+@app.route('/api/posts')
+def api_get_posts():
+    return get_posts()
 
 if __name__ == '__main__':
-    app.run()
+   app.run()
+
